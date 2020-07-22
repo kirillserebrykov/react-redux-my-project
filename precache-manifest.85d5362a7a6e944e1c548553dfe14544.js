50e3bc32c7efa9c3255dc54b9b7a1ac0a2bc7e86
Kirill Serebrykov <kirill.baza22@gmail.com>
2020-07-22 18:37:47 +0200

    Updates

---
+++ precache-manifest.85d5362a7a6e944e1c548553dfe14544.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc8c0fb1e449f4a0c674a975320bf2d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "344cafd6dcf859429682",
+    "url": "/static/css/3.6c84f38d.chunk.css"
+  },
+  {
+    "revision": "18f2c3e8914e0a4695cf",
+    "url": "/static/css/4.25d8f222.chunk.css"
+  },
+  {
+    "revision": "4655fa119aae45003596",
+    "url": "/static/css/main.8e10f0ff.chunk.css"
+  },
+  {
+    "revision": "60e8cbb69cda1ffe9963",
+    "url": "/static/js/2.adbd05ec.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.adbd05ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "344cafd6dcf859429682",
+    "url": "/static/js/3.eac6b24e.chunk.js"
+  },
+  {
+    "revision": "18f2c3e8914e0a4695cf",
+    "url": "/static/js/4.4417ef2c.chunk.js"
+  },
+  {
+    "revision": "4655fa119aae45003596",
+    "url": "/static/js/main.0ed32e05.chunk.js"
+  },
+  {
+    "revision": "303fce6902889f6e1808",
+    "url": "/static/js/runtime-main.262638e9.js"
+  },
+  {
+    "revision": "822f78a2957d26c4bbea93eda76ea3ca",
+    "url": "/static/media/loader.822f78a2.gif"
+  }
+]);
